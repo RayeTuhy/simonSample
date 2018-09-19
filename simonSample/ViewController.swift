a92f1daa968ab865e68dd00f43967d7554e49ac7
RAY ANNE TUHY <rl200485@rlas-macair-19.rlas-116.org>
2018-09-19 12:19:56 -0500

    Initial Commit

---
+++ simonSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simonSample
+//
+//  Created by RAY ANNE TUHY on 9/19/18.
+//  Copyright © 2018 RAY ANNE TUHY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
